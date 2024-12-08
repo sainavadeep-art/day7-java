8ef1090606c9a5012bbc82c38193c0db0171a268
sainavadeep-art <chittoorsainavadeep1135@gmail.com>
2024-12-08 10:44:29 +0530

    Add files via upload

---
+++ twopoint.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Rain
+{
+    public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++)
+    arr[i]=sc.nextInt();
+    int l=0;
+    int r=n-1;
+    while(1<=r)
+    {
+        int temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        l++;
+        r--;
+    }
+    System.out.println(Arrays.toString(arr));
+}
+}
