401b0c2b15245b23d881a3195d74a5eb5ef9d1fc
sainavadeep-art <chittoorsainavadeep1135@gmail.com>
2024-12-08 11:07:49 +0530

    Add files via upload

---
+++ ides.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Rain
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int N=sc.nextInt();
+	    if(N==15)
+	    {
+	        System.out.println("yes");
+	    }
+	    else{
+	        System.out.println("no");
+	    }
+	    }
+	
+	}
+
